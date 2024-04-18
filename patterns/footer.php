0bc633b7adc2b2efb1ebf186311934973c3b40e8
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2024-04-18 18:44:23 +0200

    First copy form CBT in Git

---
+++ patterns/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: footer
+ * Slug: pauli-plain/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"80px","bottom":"30px"}}}} -->
+<div class="wp-block-group" style="padding-top:80px;padding-bottom:30px"><!-- wp:separator {"backgroundColor":"custom-beige","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-custom-beige-color has-alpha-channel-opacity has-custom-beige-background-color has-background is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:social-links {"layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://twitter.com/bph","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"https://gutenbergtimes.com","service":"wordpress"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><?php echo __('Proudly Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a>', 'pauli-plain');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
